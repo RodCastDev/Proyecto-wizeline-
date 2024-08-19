f6215160e74f43f9227e8261ce103c7afc6fd5d1
Rodrigo Castiblanco Pardo <101688534+RodCastDev@users.noreply.github.com>
2024-08-19 16:41:39 -0500

    test login new proyect

---
+++ src/test/java/co/com/wizeline/project/runners.java
@@ -0,0 +1,9 @@
+package co.com.wizeline.project;
+
+import io.cucumber.core.options.CucumberOptionsAnnotationParser;
+
+public class runners {
+}
+
+
+@RunWith(CucumberOptionsAnnotationParser.CucumberOptions)
---
+++ src/test/java/co/com/wizeline/project/stepdefinitions.java
@@ -0,0 +1,4 @@
+package co.com.wizeline.project;
+public class stepdefinitions {
+}
+
